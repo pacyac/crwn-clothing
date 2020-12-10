592802bc18438d27b4325acd5e65640ba8724479
Martin Ahedor <ahedor24@gmail.com>
2020-12-10 10:03:48 -1200

    updating the app with the shop page and individual shop item

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { React } from "react";
+import { Route,Switch } from 'react-router-dom';
+
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx'; 
+
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+//<Homepage />
